fa847badfdb1779fab4913cf84fe65c0184cbb34
kirill-korolev <kafafyf@gmail.com>
2019-09-28 15:48:16 +0300

    Sqrt and inverse sqrt stub

---
+++ include/math.hpp
@@ -0,0 +1,11 @@
+#ifndef NE_MATH_H
+#define NE_MATH_H
+
+#include "types.hpp"
+
+namespace ne {
+    Float sqrt(Float x);
+    Float invSqrt(Float x);
+}
+
+#endif //NE_MATH_H
---
+++ src/math.cpp
@@ -0,0 +1,16 @@
+#include "math.hpp"
+
+#include <cmath>
+
+namespace ne {
+
+    Float sqrt(Float x){
+        //TODO: For now
+        return ::sqrt(x);
+    }
+
+    Float invSqrt(Float x){
+        //TODO: For now
+        return 1 / sqrt(x);
+    }
+}
